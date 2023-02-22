aec1b5927b67de6fba5d2f3d2da134a412c6335f
Surya Lama <yondhen123@gmail.com>
2023-02-22 20:11:20 +0900

    merge

---
+++ pythonjson.py
@@ -0,0 +1,51 @@
+import json
+# some JSON:
+x =  '{ "name":"John", "age":30, "city":"New York"}'
+
+# parse x:
+y = json.loads(x)
+
+# the result is a Python dictionary:
+print(y["age"])
+
+# a python object (dict)
+
+x={
+    "name":"Surya",
+    "age":30,
+    "city":"Tokyo"
+
+}
+
+# convert into json
+y=json.dumps(x)
+print(y)
+
+print(json.dumps({"name": "John", "age": 30}))
+print(json.dumps(["apple", "bananas"]))
+print(json.dumps(("apple", "bananas")))
+print(json.dumps("hello"))
+print(json.dumps(42))
+print(json.dumps(31.76))
+print(json.dumps(True))
+print(json.dumps(False))
+print(json.dumps(None))
+
+# formatting
+
+x = {
+  "name": "John",
+  "age": 30,
+  "married": True,
+  "divorced": False,
+  "children": ("Ann","Billy"),
+  "pets": None,
+  "cars": [
+    {"model": "BMW 230", "mpg": 27.5},
+    {"model": "Ford Edge", "mpg": 24.1}
+  ]
+}
+print(json.dumps(x, indent=4, separators=(". ", " = ")))
+
+#use sort_keys to order the result 
+#json.dumps(x, indent=4, sort_keys=True)
